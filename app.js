86077af4d47a7771f6a9131a2226175dac94901b
julianapeace <chancecordelia@gmail.com>
2018-07-06 04:15:09 -0500

    added working local postgres

--- app.js
+++ app.js
@@ -5,20 +5,8 @@ var fs = require('fs')
 var axios = require ('axios')
 const body_parser = require('body-parser');
 const importEnv = require('import-env');
-
-var DB = process.env.DATABASE_URL || "{database:'tasks'}"
-const pgp = require('pg-promise')();
-// var DB = process.env.DATABASE_URL
-const db = pgp(DB);
 const port = process.env.PORT || 8000;
-
-db.connect()
-    .then(obj => {
-        obj.done(); // success, release the connection;
-    })
-    .catch(error => {
-        console.log('ERROR:', error.message || error);
-    });
+var db = require('./queries');
 
 app.use(body_parser.urlencoded({extended: false}));
 
@@ -26,10 +14,19 @@ app.set('view engine', 'hbs');
 app.use(express.static('public'));
 
 
+
 app.get('/', function(req, res){
   response = '';
   res.render('index.hbs', {'response':response});
 });
+
+
+app.get('/api/puppies', db.getAllPuppies);
+app.get('/api/puppies/:id', db.getSinglePuppy);
+app.post('/api/puppies', db.createPuppy);
+// app.put('/api/puppies/:id', db.updatePuppy);
+// app.delete('/api/puppies/:id', db.removePuppy);
+
 
 app.get('/post/:slug', function (req, res) {
   // for links like /post/julie
