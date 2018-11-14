ed7e248a621c41ebc2a396095cb0cbd8ed0e0def
julianapeace <chancecordelia@gmail.com>
2018-11-14 05:46:12 -0600

    updated lost factom.js

--- factom.js
+++ factom.js
@@ -1,15 +1,12 @@
-//Required modules
+var firebase = require('firebase')
 const { FactomCli, Entry, Chain } = require("factom");
 var FCT_NODE = process.env.FCT_NODE;
 var FCT_PUB_SIG = process.env.FCT_PUB_SIG;
-
-
 const testChainId = '7b11a72cd69d3083e4d20137bb569423923a55696017b36f46222e9f83964679';
 
 /// Instantiating Factom
 // Default factomd connection to localhost:8088
 // and walletd connection to localhost:8089
-
 
 const cli = new FactomCli({
     factomd: {
@@ -21,27 +18,41 @@ const cli = new FactomCli({
 });
 
 
-
-//Chains and Entries
-
 ////     Starting with adding entries to the existing chain.
 ////    Asset Registration will create a unique chain for the asset
 
-function createChain(chainName) {
+function createChain(req, res, next) {
+  var token = req.headers['authorization'];
+  if (!token) return res.status(401).send({ auth: false, message: 'No token provided.' });
+  firebase.auth().signInWithCustomToken(token)
+  .then(user_login => {
+    var cleanedObject = JSON.parse(Object.keys(req.body)[0])
+    var ipfsHash = cleanedObject.ipfsHash
+    var organizationName = cleanedObject.organizationName
+    // console.log("2 Create Chain req.body: ", req.body)
+    // console.log("2 ipfshash and organization name: ", ipfsHash, organizationName)
+
     const firstEntry = Entry.builder()
         // .extId('6d79206578742069642031') // If no encoding parameter is passed as 2nd argument, 'hex' is used
-        .extId('first herc chain from server', 'utf8') // Explicit the encoding. Or you can pass directly a Buffer
-        .extId('Can have as many of these as we want', 'utf8')
-        .extId(Date.now().toString())
-        .content('The remote Herc chain creation', 'utf8')
+        .extId(organizationName, 'utf8')// Explicit the encoding. Or you can pass directly a Buffer
+        // .extId(Date.now().toString()) // Can have as many of these as you want
+        .content(ipfsHash, 'utf8')
         .build();
 
     const chain = new Chain(firstEntry);
-    cli.add(chain, FCT_PUB_SIG)
-        .then(console.log)
-        .catch(console.error);
 
+    cli.add(chain, FCT_PUB_SIG)
+        .then(response => {
+          console.log("2 factom chainId: ", response.chainId)
+          res.send(response.chainId)
+        })
+        .catch(err => {console.log(err)});
+      })
+  .catch(err => {
+    return res.status(500).send({ auth: false, message: 'Failed to authenticate token.' });
+  })
 }
+
 
 
 // Add an entry, may need to turn this into async, tests will decide.
@@ -65,35 +76,59 @@ function createEntry(req, res, next) {
         .build();
 
     cli.add(myEntry, FCT_PUB_SIG)
-        .then(console.log)
-        .catch(console.error);
+        .then(response => {
+          /*
+          { txId: 'c111f0d03a66db786a946be5e63f344a845371a106800c9a01d743d090b841bd',
+            repeatedCommit: false,
+            chainId: 'e55c0f97cdb944c45c1ad00c6784976bf697ae6faefc8a9c9f25076b2d80dd38',
+            entryHash: '2447e75a189ccee7641811cef42b80fecea28385d709c711a327e30152ec0620' }
+          */
+          console.log("Success entry in factom chain: ", response)
+          res.send(response.entryHash)
+        })
+        .catch(err => { console.log(err) });
       })
+  .catch(err => {
+    return res.status(500).send({ auth: false, message: 'Failed to authenticate token.' });
+  })
 };
-
 
 
 // Get a Single Entry
 function getEntry(entryHash) {
-    cli.getEntry(entryHash).then(
-        entry =>
-            console.log(entry)
-    )
+  var token = req.headers['authorization'];
+  if (!token) return res.status(401).send({ auth: false, message: 'No token provided.' });
+  firebase.auth().signInWithCustomToken(token)
+  .then(user_login => {
+    cli.getEntry(entryHash)
+      .then(entry => { console.log(entry) })
+      .catch(err => { console.log(err) })
+    })
+  .catch(err => {
+    return res.status(500).send({ auth: false, message: 'Failed to authenticate token.' });
+  })
 }
 
 
 // Get All the Entries
 function getAllEntries(chainId_Or_firstEntryHash) {
-    cli.getAllEntriesOfChain(chainId_Or_firstEntryHash).then(
-        entries =>
-            console.log("get all entries, chainID:", chainId_Or_firstEntryHash, "entries:", entries)
-    )
+  var token = req.headers['authorization'];
+  if (!token) return res.status(401).send({ auth: false, message: 'No token provided.' });
+  firebase.auth().signInWithCustomToken(token)
+  .then(user_login => {
+    cli.getAllEntriesOfChain(chainId_Or_firstEntryHash)
+      .then(entries => {console.log("get all entries, chainID:", chainId_Or_firstEntryHash, "entries:", entries)
+    })
+    .catch(err => {console.log(err)})
+  })
+  .catch(err => {
+    return res.status(500).send({ auth: false, message: 'Failed to authenticate token.' });
+  })
 }
 
 // FactomCli exposes the method
 // rewindChainWhile(chainId, function predicate(entry) {}
 //  function body(entry) {})
-
-
 
 // Iterating a long chain entry by entry
 async function iterateChain(chainId) {
@@ -105,27 +140,22 @@ async function iterateChain(chainId) {
 
 // Searching an entry in a chain
 async function searchChain(entryHash, searchParam) {
-    let search = true, found;
-    await cli.rewindChainWhile(entryHash,
-        () => search,
-        function (entry) {
-            if (entry.extId[0].toString() === searchParam) {
-                search = false;
-                found = entry;
-            }
-        });
-
-
+  let search = true, found;
+  await cli.rewindChainWhile(entryHash,
+    () => search,
+    function (entry) {
+      if (entry.extId[0].toString() === searchParam) {
+          search = false;
+          found = entry;
+      }
+    });
 }
 
 module.exports = {
-
     createChain: createChain,
     createEntry: createEntry,
     getEntry: getEntry,
     getAllEntries: getAllEntries,
     iterateChain: iterateChain,
     searchChain: searchChain,
-
-
 };
